f3c14f3808e28002a073d54e0c25123220b7a9f6
nandormatyas <nandor.matyas@gmail.com>
2018-03-24 11:18:27 +0100

    active button signal added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 import { DataService } from '../data.service';
 
 @Component({
@@ -10,23 +10,23 @@ import { DataService } from '../data.service';
 
     trigger('goals', [
       transition('* => *', [
-        query(':enter', style({ opacity: 0}), {optional: true}),
+        query(':enter', style({ opacity: 0 }), { optional: true }),
 
         query(':enter', stagger('400ms', [
           animate('.6s ease-in', keyframes([
-            style({opacity:0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity:.5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity:1, transform: 'translateY(0)', offset: 1})
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
           ]))
-        ]), {optional: true}),
+        ]), { optional: true }),
 
         query(':leave', stagger('1ms', [
           animate('.3s ease-in', keyframes([
-            style({opacity:1, transform: 'translateY(0)', offset: 0}),
-            style({opacity:.5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity:0, transform: 'translateY(-75%)', offset: 1})
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 })
           ]))
-        ]), {optional: true}),
+        ]), { optional: true }),
 
       ])
     ])
@@ -34,52 +34,71 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 3;
-  btnText: string = 'ADD ITEM';
-  goalText: string = 'My first goal';
-  goals = []; 
+  itemCount = 3;
+  btnText = 'ADD ITEM';
+  goalText = 'My first goal';
+  goals = [];
   hiddenxp = false;
   hiddenLang = false;
   hiddenEdu = false;
-  selected = false;
 
-  showHidexp () {
+  showHidexp() {
     this.hiddenLang = false;
     this.hiddenEdu = false;
     this.hiddenxp = !this.hiddenxp;
-    this.selected = !this.selected;
- /*    this.selected ? true : () => {
-      let active = document.getElementsByClassName('experience');
-      active.
-    } */
-    
   }
-  showHideLang () {
+
+  showHideLang() {
     this.hiddenxp = false;
     this.hiddenEdu = false;
     this.hiddenLang = !this.hiddenLang;
   }
-  showHideEdu () {
+  showHideEdu() {
     this.hiddenxp = false;
     this.hiddenLang = false;
     this.hiddenEdu = !this.hiddenEdu;
   }
-  
+
   constructor(private _data: DataService) { }
-  
-  ngOnInit () {
+
+  activeXp() {
+    if (this.hiddenxp) {
+      return {
+        'color': '#8ef38e',
+        'box-shadow': 'unset'
+      };
+    }
+  }
+  activeLang() {
+    if (this.hiddenLang) {
+      return {
+        'color': '#8ef38e',
+        'box-shadow': 'unset'
+      };
+    }
+  }
+  activeEdu() {
+    if (this.hiddenEdu) {
+      return {
+        'color': '#8ef38e',
+        'box-shadow': 'unset'
+      };
+    }
+  }
+
+  ngOnInit() {
     this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
-  addItem () {
+  addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
-  removeItem (i) {
+  removeItem(i) {
     this.goals.splice(i, 1);
     this._data.changeGoal(this.goals);
 
